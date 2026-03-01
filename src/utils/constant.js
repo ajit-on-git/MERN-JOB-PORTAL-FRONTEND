f399a63215636844f310da70f61893b9f1e9ce7f
ajit-on-git <kumarajitp58@gmail.com>
2026-03-01 12:42:25 +0530

    job description resolved

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,15 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT = "http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+const BASE_URL = "https://mern-job-portal-backend-039n.onrender.com";
+
+export const USER_API_END_POINT = `${BASE_URL}/api/v1/user`;
+export const JOB_API_END_POINT = `${BASE_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${BASE_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT = `${BASE_URL}/api/v1/company`;
+
+/*
+https://mern-job-portal-backend-039n.onrender.com/
+
+export const USER_API_END_POINT = "https://your-backend-name.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://your-backend-name.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://your-backend-name.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://your-backend-name.onrender.com/api/v1/company";
+*/
